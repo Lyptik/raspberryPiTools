c161b764b601ed7aa4da3d2cff3ef25fe69e0e92
David Chanel <david@theoriz.com>
2015-03-05 17:19:14 +0000

    readonly script WIP

---
+++ scripts/tools/readonly.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+# This script changes your raspberry pi as a readonly system
+# Use it at your own risk
+
+# Error out if anything fails
+set -e
+
+# Make sure script is run as root
+if [ "$(id -u)" != "0" ]; then
+	echo "Must be run as root ! Try : sudo $0"
+	exit 1
+fi
+
+echo "Modifying ftab and config to mount partition in readonly"
+
+echo "RAMTMP=yes" >> /etc/default/rcS
+
+sed -i '2s/defaults/defaults,ro/' /etc/fstab
+sed -i '3s/noatime/noatime,ro/' /etc/fstab
+
+echo "tmpfs           /tmp            tmpfs   nodev,nosuid,size=30M,mode=1777    0    0
+tmpfs           /var/log           tmpfs   nodev,nosuid,size=30M,mode=1777    0    0" >> fstab
+
+sed -i '60s/-f/-L/' /etc/init.d/hwclock.sh
+
+echo 'BLKID_FILE="/var/local/blkid.tab"' >> /etc/environment
+
+rm /etc/mtab
+ln -s /proc/self/mounts /etc/mtab
+
+echo "Not using resolv.conf standard way for dns resolving..."
+rm -rf /etc/network/run
+dpkg-reconfigure ifupdown
+apt-get update
+apt-get install resolvconf
+
+if [ -f /etc/init.d/supervisor ]; then
+	
+	echo "You seem to have supervisor installed, Adding a fix enabling supervisor to work correctly with a readonly system"
+	sed -i '95 i\        mkdir -p /var/log/supervisor' /etc/init.d/supervisor
+else
+	echo "  /!\ WARNING : You do not have supervisor installed, if you want to install it later, you will probably need the hotfix contained in this script"
+fi
+
+echo "Installing tools to easy remount in readonly or readwrite"
+
+echo "mount -o remount,rw /" >> /usr/local/bin/remountrw
+chmod +x /usr/local/bin/remountrw
+
+echo "mount -o remount,ro /" >> /usr/local/bin/remountro
+chmod +x /usr/local/bin/remountro
+
+echo "You can now use remountrw and remountro command, have fun !"
+
+echo "Done."
+
